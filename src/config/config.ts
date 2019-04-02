2e646453945ee8f6617dad4cfc907372231cfb15
apokalupsis <26.apokalupsis@gmail.com>
2019-04-02 07:22:41 +0200

    add https

--- src/config/config.ts
+++ src/config/config.ts
@@ -2,8 +2,10 @@ import {Configuration} from 'tsunamy/core';
 
 export let CONFIGURATION: Configuration = {
   hostname : '127.0.0.1',
-  port: 3000,
+  http: true,
+  httpPort: 8080,
   https: false,
+  httpsPort: 8000,
   pathAPI: 'api',
   projectDirectory: __dirname + '/..'
 };
